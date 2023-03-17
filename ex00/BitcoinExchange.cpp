fff60baf0c93485974828a19899d1aeb58ad4fcf
brixxt27 <brixxt27@gmail.com>
2023-03-17 14:18:47 +0900

    feat: BitcoinExchange 설계#2
    multimap 의 value 값을 int가 아닌 float으로 해야한다는 것을 깨달았고,
    mValue 와 mExchageRate 를 곱한 값을 출력하는 함수를 설계 중

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -10,6 +10,22 @@ BitcoinExchange::~BitcoinExchange()
 	
 }
 
+std::multimap<std::string, double>&	BitcoinExchange::getValue()
+{
+	return value;
+}
+
+std::multimap<std::string, double>&	BitcoinExchange::getExchangeRate()
+{
+	return exchange_rate;
+}
+
+void	BitcoinExchange::printMultipledResult() const
+{
+	//std::cout << value.
+}
+
+
 BitcoinExchange::BitcoinExchange(const BitcoinExchange& other)
 {
 
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -1,11 +1,12 @@
 #ifndef BITCOINEXCHANGE
 #define BITCOINEXCHANGE
 
-#define MSG_ERR_NOT_OPEN "Error: could not open file."
-#define MSG_ERR_NOT_APPROPRIATE_ARGUMENTS "Error: There are not appropriate arguments"
-#define MSG_ERR_NOT_OPEN_CSV_FILE "Error: There is not a data.csv. Please download a file!"
+#define MSG_ERR_NOT_OPEN					"Error: could not open file."
+#define MSG_ERR_NOT_APPROPRIATE_ARGUMENTS	"Error: There are not appropriate arguments"
+#define MSG_ERR_NOT_OPEN_CSV_FILE			"Error: There is not a data.csv. Please download a file!"
 
 #include <map>
+#include <iostream>
 
 class BitcoinExchange
 {
@@ -13,13 +14,18 @@ public:
 	BitcoinExchange();
 	~BitcoinExchange();
 
+	std::multimap<std::string, double>&	getValue();
+	std::multimap<std::string, double>&	getExchangeRate();
+
+	void	printMultipledResult() const;
+	//double	multiplyValueAndExchangeRate() const;
+
 private:
 	BitcoinExchange(const BitcoinExchange& other);
 	BitcoinExchange&	operator=(const BitcoinExchange& rhs);
 
-
-	std::multimap<std::string, int>	input;
-	std::multimap<std::string, int>	csv;
+	std::multimap<std::string, double>	value;
+	std::multimap<std::string, double>	exchange_rate;
 };
 
 #endif
